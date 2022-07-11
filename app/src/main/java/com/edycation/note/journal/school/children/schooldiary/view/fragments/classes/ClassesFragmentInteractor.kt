9cad8875507c867ef6e2047612efc24edee6174b
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-07-11 16:23:40 +0300

    Скорректирована работа списка домашних заданий с использованием "Paging 3.0" в рамках архитектуры MVVM (через observe).

--- app/src/main/java/com/edycation/note/journal/school/children/schooldiary/view/fragments/classes/ClassesFragmentInteractor.kt
+++ app/src/main/java/com/edycation/note/journal/school/children/schooldiary/view/fragments/classes/ClassesFragmentInteractor.kt
@@ -3,12 +3,9 @@ package com.edycation.note.journal.school.children.schooldiary.view.fragments.cl
 import com.edycation.note.journal.school.children.schooldiary.model.base.Interactor
 import com.edycation.note.journal.school.children.schooldiary.model.data.AppState
 import com.edycation.note.journal.school.children.schooldiary.repository.settings.Settings
-import com.edycation.note.journal.school.children.schooldiary.utils.resources.ResourcesProvider
 import org.koin.java.KoinJavaComponent.getKoin
 
-class ClassesFragmentInteractor(
-    private val resourcesProviderImpl: ResourcesProvider,
-): Interactor<AppState> {
+class ClassesFragmentInteractor: Interactor<AppState> {
     /** Исходные данные */ //region
     private val settings: Settings = getKoin().get()
     //endregion
